61b255b233e47a798a166e3fe67f00e0028c5f6e
Nahuel Gomez Suarez <nahuelgomezs@outlook.com>
2023-08-02 09:17:08 -0300

    Format with Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css';
-import { Container } from '@mui/material';
-import Form from './components/Form';
+import "./App.css";
+import { Container } from "@mui/material";
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <Container>
-      <h1>Form handling with Formik</h1>
-      <Form />
-    </Container>
-  )
+    return (
+        <Container>
+            <h1>Form handling with Formik</h1>
+            <Form />
+        </Container>
+    );
 }
 
-export default App
+export default App;
