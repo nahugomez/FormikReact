2756a70e53771e498b143e3aa5ac1ef4fd60fa52
Nahuel Gomez Suarez <nahuelgomezs@outlook.com>
2023-08-03 11:58:05 -0300

    Error Boundary Added

---
+++ src/utils/ErrorBoundary.jsx
@@ -0,0 +1,34 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: false,
+            errorMessage: "",
+        };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { error: true, errorMessage: error.errorMessage };
+    }
+
+    render() {
+        if (this.state.error) {
+            return (
+                <Box>
+                    <p>Ocurrió un error, contacte con soporte</p>
+                    <p>{this.state.errorMessage}</p>
+                    <Button onClick={() => (window.location.href = "/")} variant="outlined">
+                        Recargar página
+                    </Button>
+                </Box>
+            );
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
